4d474d875a1885501faef34a60ba1289befed4a2
mohd-sahal <mohdsahal6060@gmail.com>
2025-04-20 21:35:33 +0400

    Add files via upload

---
+++ PortScanner.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+import re
+from scapy.all import *
+
+try:
+    host = input("Enter a host address: ")
+    p = list(input("Enter the ports to scan: ").split(","))
+    temp = map(int, p)
+    ports = list(temp)
+
+    if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", host):
+        print("\n\nScanning...")
+        print("Host: ", host)
+        print("Ports: ", ports)
+
+        ans, unans = sr(IP(dst=host)/TCP(dport=ports, flags="S"), verbose=0, timeout=2)
+
+        for (s, r) in ans:
+            print("[+] {} Open".format(s[TCP].dport))
+
+except (ValueError, RuntimeError, TypeError, NameError):
+    print("[-] Some Error Occurred")
+    print("[-] Exiting..")
